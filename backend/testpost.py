c485c8d678da145fe3024800fd7e2b9302540112
SRaswan <shauryaraswan@gmail.com>
2024-01-20 18:31:55 -0800

    Final testing weights done

--- backend/testpost.py
+++ backend/testpost.py
@@ -1,6 +1,10 @@
 from asl import video_to_text
 
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled1.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled2.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled3.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled4.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled5.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled6.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled7.mov'))
