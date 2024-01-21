60f982953a217f0f38f51022d8bb3ad5aa1a3acf
SRaswan <shauryaraswan@gmail.com>
2024-01-21 08:20:28 -0800

    Deleted 2 words

--- backend/testpost.py
+++ backend/testpost.py
@@ -1,17 +1,19 @@
 from asl import video_to_text
 
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled1.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled2.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled3.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled4.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled5.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled6.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled7.mov'))
-# print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled9.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled1.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled2.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled3.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled4.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled5.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled6.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled7.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled9.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled11.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled12.mov'))
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled13.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/IMG_6127.mov'))
+
 
 
 
