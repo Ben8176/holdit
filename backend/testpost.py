e18c2a4d6833cbbe8826809f8bb1d900b17e81cd
SRaswan <shauryaraswan@gmail.com>
2024-01-20 17:31:33 -0800

    Adding asl code

--- backend/testpost.py
+++ backend/testpost.py
@@ -1,3 +1,6 @@
-from asl3 import video_to_text
+from asl import video_to_text
 
 print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled1.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled4.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled5.mov'))
+print(video_to_text('/Users/sraswan/Documents/Workspace/holdit/Untitled6.mov'))
